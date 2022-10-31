d2507b422e02f6806d0d3cf45752a9e594555b0b
todayIsDecember <95939170+todayIsDecember@users.noreply.github.com>
2022-10-31 02:11:29 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const menu_kichen = document.querySelector('.menu_kichen');
 const menu_categories_kichen = document.querySelector('.menu_categories_kichen');
 const menu_bar = document.querySelector('.menu_bar');
 const menu_categories_bar = document.querySelector('.menu_categories_bar');
+const menu_first_categories = document.querySelector('.menu_first_categories');
+const menu_salat_categories = document.querySelector('.menu_salat_categories');
 
 menu_kichen.addEventListener('click', onClickKichenHandler);
 
@@ -16,3 +18,12 @@ menu_bar.addEventListener('click', onCkickBarHandler);
 function onCkickBarHandler() {
   menu_categories_bar.classList.toggle('active');
 }
+
+menu_categories_kichen.addEventListener('click', (e) => {
+  let parent = e.target.closest('.menu_submenu');
+  if(parent.classList.contains('menu_first')) {
+    menu_first_categories.classList.toggle('active');
+  }else if(parent.classList.contains('menu_salats')){
+    menu_salat_categories.classList.toggle('active');
+  }
+})
